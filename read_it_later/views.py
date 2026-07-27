c346b5c407bfad02f9930d5b0ad85f43e90d7c31
Toluwanimi <tolu792@gmail.com>
2026-07-27 14:38:23 +0100

    Implement article addition feature with form and URL handling

--- read_it_later/views.py
+++ read_it_later/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+import requests
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.db import IntegrityError
+from django.shortcuts import redirect, render
 
-# Create your views here.
+from .forms import AddArticleForm
+from .models import Article
+from .services import fetch_article_metadata
+
+
+@login_required
+def add_article(request):
+    if request.method == "POST":
+        form = AddArticleForm(request.POST)
+        if form.is_valid():
+            url = form.cleaned_data["url"]
+            try:
+                metadata = fetch_article_metadata(url)
+            except requests.RequestException:
+                messages.error(request, "Couldn't fetch that URL. Check the link and try again.")
+            else:
+                try:
+                    Article.objects.create(user=request.user, url=url, **metadata)
+                except IntegrityError:
+                    messages.warning(request, "You've already saved that article.")
+                else:
+                    messages.success(request, "Article saved.")
+                    return redirect("add_article")
+    else:
+        form = AddArticleForm()
+
+    return render(request, "read_it_later/add_article.html", {"form": form})

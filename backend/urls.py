c48d6c4b8ae77e4f1bc7df9e527ac381f99d9698
Toluwanimi <tolu792@gmail.com>
2026-07-27 15:49:16 +0100

    Add authentication views and update templates for login and logout

--- backend/urls.py
+++ backend/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('', include('read_it_later.urls'))
 ]

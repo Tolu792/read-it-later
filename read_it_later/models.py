532d6b348765fc6e797d9d17547927b14672b499
Toluwanimi <tolu792@gmail.com>
2026-07-27 11:57:51 +0100

    Add Tag and Article models with migrations

--- read_it_later/models.py
+++ read_it_later/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.text import slugify
 
 # Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(unique=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    class Meta:
+        db_table = "Tag"
+        managed = True
+
+
+class Article(models.Model):
+    class Status(models.TextChoices):
+        UNREAD = 'Unread'
+        READ = 'Read'
+        ARCHIVED = 'Archived'
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='articles')
+    url = models.URLField(max_length=2000)
+    image_url = models.URLField(max_length=2000, blank=True)
+    title = models.CharField(max_length=255, blank=True)
+    description = models.CharField(max_length=300, blank=True)
+    content_text = models.TextField(blank=True)
+    reading_time_minutes = models.PositiveIntegerField(default=0)
+    status = models.CharField(choices=Status.choices, default=Status.UNREAD)
+    tags = models.ManyToManyField(Tag, blank=True, related_name='articles')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = "Article"
+        managed = True
+        unique_together = ('user', 'url')
+        ordering = ['-created_at']

85e39bfa046722cc29a21d6e9fd892b26f3677e3
Toluwanimi <tolu792@gmail.com>
2026-07-27 12:07:37 +0100

    Register Tag and Article models in the Django admin interface

--- read_it_later/admin.py
+++ read_it_later/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Tag, Article
 
 # Register your models here.
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'status', 'reading_time_minutes', 'created_at')
+    list_filter = ('status', 'tags')
+    search_fields = ('title', 'url', 'description')
+    filter_horizontal = ('tags',)
